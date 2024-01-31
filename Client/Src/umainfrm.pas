33f226d66cdbf229e74c612ea8da846b3722b8d0
Enzo <enzoocezargarcia@gmail.com>
2024-01-31 10:48:50 -0300

    feat: added-rx-tools-binary-ds

--- Client/Src/umainfrm.pas
+++ Client/Src/umainfrm.pas
@@ -5,24 +5,31 @@ unit uMainFrm;
 interface
 
 uses
-  Classes, SysUtils, DB, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  DBGrids, RxDBGrid, ZDataset;
+  Classes, SysUtils, DB, BufDataset, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, StdCtrls, DBGrids, RxDBGrid, rxmemds, ZDataset;
 
 type
 
   { TMainFrm }
 
   TMainFrm = class(TForm)
+    BufDataset: TBufDataset;
     Button1: TButton;
     Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
     dsQuery: TDataSource;
     dsMemTbl: TDataSource;
     dbgMemTbl: TDBGrid;
     dbgQuery: TDBGrid;
     mmJson: TMemo;
     pnlTop: TPanel;
+    RxMemoryData: TRxMemoryData;
     ZQuery: TZQuery;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormResize(Sender: TObject);
   private
@@ -61,5 +68,61 @@ begin
   ZQuery.Active := not ZQuery.Active;
 end;
 
-end.
+procedure TMainFrm.Button2Click(Sender: TObject);
+begin
+  dsMemTbl.DataSet.Active := not dsMemTbl.DataSet.Active;
+end;
 
+procedure TMainFrm.Button3Click(Sender: TObject);
+var
+   lStream: TMemoryStream;
+   lBuf: TBufDataSet;
+begin
+
+  if BufDataSet.Active then
+     BufDataSet.Close;
+
+  // BufDataSet.CopyFromDataset( ZQuery );
+
+  lStream := TMemoryStream.Create;
+  lBuf := TBufDataSet.Create(nil);
+  try
+    lBuf.SaveToStream( lStream, dfBinary );
+
+    lStream.Position := 0;
+
+    BufDataSet.LoadFromStream( lStream, dfBinary );
+  finally
+    FreeAndNil(lStream);
+    lBuf.Close;
+    FreeAndNil(lBuf);
+  end;
+
+end;
+
+procedure TMainFrm.Button4Click(Sender: TObject);
+var
+   lStream: TMemoryStream;
+   lMtb: TRxMemoryData;
+begin
+  dsMemTbl.DataSet := RxMemoryData;
+
+  if RxMemoryData.Active then
+    RxMemoryData.Close;
+
+  lStream := TMemoryStream.Create;
+  lMtb := TRxMemoryData.Create(nil);
+  try
+    lMtb.LoadFromDataSet( ZQuery, 0, lmCopy );
+    lMtb.SaveToStream( lStream );
+    lStream.Position := 0;
+    RxMemoryData.LoadFromStream( lStream );
+  finally
+    FreeAndNil(lStream);
+    lMtb.Close;
+    FreeAndNil(lMtb);
+  end;
+  // RxMemoryData.LoadFromDataSet( ZQuery, 0, lmCopy );
+end;
+
+end.
